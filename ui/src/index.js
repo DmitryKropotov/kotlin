480c3d1e7d3e106e11d30c8778a2d44d556fcfd8
DKropotov <kropotovd@gmail.com>
2022-01-19 18:07:21 +0300

    added ui (react)

---
+++ ui/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ShoppingList } from './ShoppingList.js';
+
+ReactDOM.render(
+    <ShoppingList />,
+    document.getElementById('root')
+)
---
+++ ui/src/main.js
@@ -0,0 +1,1 @@
+document.getElementById("demo").innerHTML = "not hard coded script";
