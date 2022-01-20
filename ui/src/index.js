f3b2631d45ef7c7a993316f0b666210e4b68a6aa
DKropotov <kropotovd@gmail.com>
2022-01-20 12:12:26 +0300

    added game and some interaction

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ShoppingList } from './ShoppingList.js';
+import { Game } from './Game.js';
+
+/*ReactDOM.render(
+    <ShoppingList />,
+    document.getElementById('root')
+)*/
 
 ReactDOM.render(
-    <ShoppingList />,
+    <Game />,
     document.getElementById('root')
 )
